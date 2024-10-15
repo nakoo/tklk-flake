39f838f4d317fce871ca176d8d39e9c57f0c2a2c
techknowlogick <techknowlogick@gitea.com>
2024-10-15 11:27:52 -0400

    bump hashi tools

--- flake.nix
+++ flake.nix
@@ -106,6 +106,81 @@
                 hash = "sha256-NpMBYqk99BfZC9IsbhTa/0cFuqwrAkGO3aZxzfqc0H8=";
               };
             });
+            # temporary, until PR #348657 is built in nixpkgs-unstable
+            consul = super.consul.overrideAttrs (oldAttrs: rec {
+              version = "1.20.0";
+              vendorHash = "sha256-7Nw2zuTyAR7mzxFkeOuhbh9OAlshZA0JKOVQdckIF90=";
+              src = fetchFromGitHub {
+                owner = "hashicorp";
+                repo = "consul";
+                rev = "refs/tags/v${version}";
+                hash = "sha256-yHhaaZZ/KxQk8RVkqNfyfWTPS5K+BhckcxqdC5gN+ko=";
+              };
+            });
+            # temporary, until PR #347416 is built in nixpkgs-unstable
+            vault = super.vault.overrideAttrs (oldAttrs: rec {
+              version = "1.18.0";
+              src = super.fetchFromGitHub {
+                owner = "hashicorp";
+                repo = "vault";
+                rev = "v${version}";
+                hash = "sha256-5CqA2dZZdV1IiGSGwCA2eQIhp3lrsDIJt4rDK1vdvmE=";
+              };
+              vendorHash = "sha256-2txRuunh6x+iDKRpljGpSX6Q8q11a84CPVm6d299NNY=";
+            });
+            vault-bin = super.vault-bin.overrideAttrs (oldAttrs: rec {
+              version = "1.18.0";
+              src =
+                let
+                  inherit (super.stdenv.hostPlatform) system;
+                  selectSystem = attrs: attrs.${system} or (throw "Unsupported system: ${system}");
+                  suffix = selectSystem {
+                    x86_64-linux = "linux_amd64";
+                    aarch64-linux = "linux_arm64";
+                    i686-linux = "linux_386";
+                    x86_64-darwin = "darwin_amd64";
+                    aarch64-darwin = "darwin_arm64";
+                  };
+                  hash = selectSystem {
+                    x86_64-linux = "sha256-fyVkSZ20tUcBv9/iT1h3o/2KkoCJ5op7DBoMc0US7SM=";
+                    aarch64-linux = "sha256-Vsc0ra+OzrDBwmKke0ef4kfy5CWu5m34gC7u0BDL7uo=";
+                    i686-linux = "sha256-3uAkBPOoMbdfS5EfII03JbVl1ekfRXm4yv1rL5A7x7c=";
+                    x86_64-darwin = "sha256-fydYqDEihbGuZ9I1quJSJk+lJxnSkqF+t1mOP8EA2Ok=";
+                    aarch64-darwin = "sha256-yJmNM9eQydbRdY6+JK28hhzXJ9Hj3CcwUJkhS60aCyA=";
+                  };
+                in
+                super.fetchzip {
+                  url = "https://releases.hashicorp.com/vault/${version}/vault_${version}_${suffix}.zip";
+                  stripRoot = false;
+                  inherit hash;
+                };
+            });
+            # temporary, until PR #348614 is built in nixpkgs-unstable
+            boundary = super.boundary.overrideAttrs (oldAttrs: rec {
+              version = "0.18.0";
+              src =
+                let
+                  inherit (super.stdenv.hostPlatform) system;
+                  selectSystem = attrs: attrs.${system} or (throw "Unsupported system: ${system}");
+                  suffix = selectSystem {
+                    x86_64-linux = "linux_amd64";
+                    aarch64-linux = "linux_arm64";
+                    x86_64-darwin = "darwin_amd64";
+                    aarch64-darwin = "darwin_arm64";
+                  };
+                  hash = selectSystem {
+                    x86_64-linux = "sha256-Wp1gPFQkOv+ZCEy0D2Tw9l6aCZekdpkXYcTZNheJHEg=";
+                    aarch64-linux = "sha256-jBYu4m3L+j/coJ4D9cPA8mSBYiLiUyVKp98x6mdrrrk=";
+                    x86_64-darwin = "sha256-OuiF1pgutt69ghlkLkEwkWMIFjvAsY7YUZERHNiToMs=";
+                    aarch64-darwin = "sha256-sYKA02euri/K8FM8GoY7Y/WWLE2nBSoiNoxSdUPunWA=";
+                  };
+                in
+                super.fetchzip {
+                  url = "https://releases.hashicorp.com/boundary/${version}/boundary_${version}_${suffix}.zip";
+                  inherit hash;
+                  stripRoot = false;
+                };
+            });
           })
           (self: super: {
             # change default to 1.8
