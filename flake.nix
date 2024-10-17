13c821d0a6fc71afade5ceaae3dbff049166a2d1
techknowlogick <techknowlogick@gitea.com>
2024-10-16 22:58:36 -0400

    nomad-pack bumped upstream

--- flake.nix
+++ flake.nix
@@ -96,17 +96,6 @@
             });
           })
           (self: super: {
-            # temporary, until PR #344262 is merged
-            nomad-pack = super.nomad-pack.overrideAttrs (oldAttrs: rec {
-              version = "nightly-2024-09-23";
-              src = super.fetchFromGitHub {
-                owner = "hashicorp";
-                repo = "nomad-pack";
-                rev = "3c0178a561b360906c591718edb25ae25ae9d964"; # nightly tag as of 2024-09-23
-                hash = "sha256-8erw+8ZTpf8Dc9m6t5NeRgwOETkjXN/wVhoZ4g0uWhg=";
-              };
-              vendorHash = "sha256-Rt0T6cCMzO4YBFF6/9xeCZcsqziDmxPMNirHLqepwek=";
-            });
             # temporary, until PR #348657 is built in nixpkgs-unstable
             consul = super.consul.overrideAttrs (oldAttrs: rec {
               version = "1.20.0";
